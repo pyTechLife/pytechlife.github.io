8081219db9d9fd4322dac4decfe335ff663f02af
mojtaba <mojtaba.parvar90@gmail.com>
2025-05-01 09:37:20 +0330

    Update service-worker.js

--- panel/service-worker.js
+++ panel/service-worker.js
@@ -1,17 +1,19 @@
-const CACHE_NAME = 'my-pwa-cache-v1';
-const STATIC_FILES = [
-  '/',
-  '/index.html',
+const CACHE_NAME = "sample-pwa-cache-v1";
+const urlsToCache = [
+  "/sample/",
+  "/sample/index.html"
 ];
-
-self.addEventListener('install', (event) => {
+self.addEventListener("install", function(event) {
   event.waitUntil(
-    caches.open(CACHE_NAME).then(cache => cache.addAll(STATIC_FILES))
+    caches.open(CACHE_NAME).then(function(cache) {
+      return cache.addAll(urlsToCache);
+    })
   );
 });
-
-self.addEventListener('fetch', (event) => {
+self.addEventListener("fetch", function(event) {
   event.respondWith(
-    caches.match(event.request).then(res => res || fetch(event.request))
+    caches.match(event.request).then(function(response) {
+      return response || fetch(event.request);
+    })
   );
 });
